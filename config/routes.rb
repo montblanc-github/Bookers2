e14681e10f7d91679f808610b4825cb3d0d004a0
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-06 09:32:30 +0000

    [Add] header,footerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit, :update]
+  get "/home/about" =>"homes#about"
   root to: "homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
