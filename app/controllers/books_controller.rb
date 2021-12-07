2575e1f84fe31fb67172de95f288ff3ea99c7fde
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-07 09:43:59 +0000

    [Add] 投稿一覧画面の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to book_path(@book.id)
   end
 
   def index
@@ -14,7 +14,9 @@ class BooksController < ApplicationController
   end
 
   def show
-
+    @book_new = Book.new
+    @book = Book.find(params[:id])
+    @user = User.find(@book.user.id)
   end
 
   def edit
@@ -22,6 +24,9 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 private
