f3e803782dc6ad6ef37b9e0ae2687187fd79d021
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-08 09:50:14 +0000

    [Add] バリデーション

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b1f2cf101535efa7367ae377cc78533c186379ebab3e053389e6f0c57b561121fc8f87c2682f4739c44bfb750da46ecaeeb2283c11487f9ccb5bf517bbc4a9a8'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books, only: [:create, :index, :show, :edit, :destroy]
-  resources :users, only: [:show, :edit, :update, :index]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :index, :edit, :update]
   get "/home/about" =>"homes#about"
   root to: "homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
