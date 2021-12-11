cd8f90d0a39207520b3defba1f1b5e1a5e86130a
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-11 04:09:25 +0000

    [Add]応用編課題3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :index, :edit, :update]
   get "/home/about" =>"homes#about"
   root to: "homes#top"
