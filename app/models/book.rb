ee5d106e8870a145f610e6e64880ffdbe51aa9db
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-11 04:17:08 +0000

    [Add] いいね機能コントローラー/モデル作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,11 @@
 class Book < ApplicationRecord
 
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user_id).exist?
+  end
 
   validates :title,
     presence: true
