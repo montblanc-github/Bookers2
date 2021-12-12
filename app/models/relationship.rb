45c95e4940d6ae7aef01999f441cfbb0761b8683
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-12 07:58:07 +0000

    [Add] アソシエーション

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,7 @@
 class Relationship < ApplicationRecord
+
+# 正しくuserテーブルを参照させるため、class_nameを指定
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  # フォローした、フォローされたの関係
+  # foreign_key（FK）には、@user.xxxとした際に「@user.idがfollower_idなのかfollowed_idなのか」を指定。
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_elationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # @user.booksのように、@user.yyyで、
+  # そのユーザがフォローしている人orフォローされている人の一覧を出したい
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   attachment :profile_image
 
