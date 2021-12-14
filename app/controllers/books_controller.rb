7671d6fbdbfc7e29f97e48ddd6ffd199a23dea33
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-14 06:34:06 +0000

    [Add] 投稿削除エラー解消

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -58,4 +58,12 @@ private
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
+    def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
+  end
+
 end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,25 +10,25 @@ class SearchesController < ApplicationController
 
   private
   def search_for(model, content, method)
-    if model == 'user'
-      if method == 'perfect'
+    if model == "user"
+      if method == "perfect"
         User.where(name: content)
-      elsif method == 'forward'
-        User.where('name LIKE ?', content+'%')
-      elsif method == 'backward'
-        User.where('name LIKE ?', '%'+content )
-      elsif method == 'partial'
-        User.where('name LIKE ?', '%'+content+'%')
+      elsif method == "forward"
+        User.where("name LIKE ?", content+"%")
+      elsif method == "backward"
+        User.where("name LIKE ?", "%"+content )
+      elsif method == "partial"
+        User.where("name LIKE ?", "%"+content+"%")
       end
-    elsif model == 'book'
-      if method == 'perfect'
+    elsif model == "book"
+      if method == "perfect"
         Book.where(title: content)
-      elsif method == 'forward'
-        Book.where('title LIKE ?', content+'%')
-      elsif method == 'backward'
-        Book.where('title LIKE ?', '%'+content )
-      elsif method == 'partial'
-        Book.where('title LIKE ?', '%'+content+'%')
+      elsif method == "forward"
+        Book.where("title LIKE ?", content+"%")
+      elsif method == "backward"
+        Book.where("title LIKE ?", "%"+content )
+      elsif method == "partial"
+        Book.where("title LIKE ?", "%"+content+"%")
       end
     end
   end
