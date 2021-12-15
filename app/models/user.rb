35e67e996ba8adfcdfe2c79fe99f5ffad05af9a8
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-15 14:00:43 +0000

    [Add] DM機能ボタンレイアウト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
   # フォローした、フォローされたの関係
   # foreign_key（FK）には、@user.xxxとした際に「@user.idがfollower_idなのかfollowed_idなのか」を指定。
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
