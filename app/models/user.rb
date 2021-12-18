a46aae9f22479270cecb1db60a8a039765393d73
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-18 12:55:17 +0000

    [Add] 閲覧数アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
+  has_many :view_counts, dependent: :destroy
 
   attachment :profile_image
 
