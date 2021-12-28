1f41a03f6ce15a59bf620514c92e1f271824e40b
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-28 09:15:41 +0000

    [Fix] user未定義のメソッド定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,4 +39,10 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
 end
