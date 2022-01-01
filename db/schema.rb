233ec83c392b7d7009aae0ec5e4af1a996f28880
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-01-01 09:25:24 +0000

    [Update] Rubocop/一意性バリデーション追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_18_124239) do
+ActiveRecord::Schema.define(version: 2022_01_01_091152) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2021_12_18_124239) do
     t.string "introduction"
     t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
