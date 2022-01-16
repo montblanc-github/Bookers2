2a6597be7dab0e1e6463ae29ef428981679fec4e
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-01-16 12:40:06 +0000

    :construction: コントローラーにアクション追加
    コントローラーにmail関連のアクションを追加したため。

--- app/mailers/group_mailer.rb
+++ app/mailers/group_mailer.rb
@@ -1,4 +1,10 @@
 class GroupMailer < ApplicationMailer
   default from: '管理人<ENV["KEY"]>'
   layout 'mailer'
+
+  def send_mail(mail_title,mail_content,group_users)
+    @mail_title = mail_title
+    @mail_content = mail_content
+    mail bcc: group_users.pluck(:email), subject: mail_title
+  end
 end
