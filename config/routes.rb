99f22b994e55670b42f1c30d708cd1b9f208d208
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2022-01-16 13:20:49 +0000

    :sparkles: グループメール機能追加
    グループメンバーにメールを送る機能追加。

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,10 @@ Rails.application.routes.draw do
 
   resources :groups do
     get "join"
+		get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
   end
+
   get "message/:id", to: "messages#show", as: "message"
   resources :messages, only: [:create]
   get "/home/about" => "homes#about"
